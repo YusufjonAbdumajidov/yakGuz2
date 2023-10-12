bf1b20fb0a1e1b99f3ad2f61d82ae4802a450ac2
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-10-12 19:31:41 +0300

    many things added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 
+import React from "react";
+import "./style.scss";
+import { Routes, Route } from "react-router-dom";
+import Malumotlar from "./Components/Malumotlar";
+import Home from "./Components/Home";
+import Qabul from "./Components/Qabul";
+
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/qabul" element={<Qabul />} />
+
+        <Route path="/malumotlar" element={<Malumotlar/>} />
+      </Routes>
+      {/* <Main /> */}
     </div>
   );
 }
---
+++ src/Components/Malumotlar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Malumotlar = () => {
+  return (
+    <div>Malumotlar</div>
+  )
+}
+
+export default Malumotlar
